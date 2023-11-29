20c19680e1187fac29769594a7cb070cdb802163
Nayan Patidar <nayanpatidar28@gmail.com>
2023-11-29 21:49:46 +0530

    Tic-Tac-Toe

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -1,1 +1,7 @@
+let valuesArray = [0, 0];
 
+    // Function to update the array value and the box text
+    function updateValue(index) {
+      valuesArray[index]++;
+      document.getElementById(`box${index + 1}`).innerText = `${valuesArray[index]}`;
+    }
