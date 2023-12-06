3a7804715bdc62db03da36ce59bb71ec00b44bf4
NayanPatidar <nayanpatidar28@gmail.com>
2023-12-06 16:41:47 +0530

    JSBasics

--- COHORT/class-1/main.js
+++ COHORT/class-1/main.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+let userName = "Nayan"
+
+if (userName == true){
+    console.log(`Hello ${userName}`);
+}
+
+let num = [1,4,0,2,4,9,2,2]
+for (const element in num) {
+    console.log(num[element]);
+}
+
