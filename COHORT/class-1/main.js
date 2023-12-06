aeccc0abea34e2bf755bd68d924948ae13b4f66e
NayanPatidar <nayanpatidar28@gmail.com>
2023-12-06 20:11:51 +0530

    JSON understanding

--- COHORT/class-1/main.js
+++ COHORT/class-1/main.js
@@ -1,11 +1,25 @@
-let userName = "Nayan"
+// let userName = "Nayan"
 
-if (userName == true){
-    console.log(`Hello ${userName}`);
+// if (userName == true){
+//     console.log(`Hello ${userName}`);
+// }
+
+// let num = [1,4,0,2,4,9,2,2]
+// for (const element in num) {
+//     console.log(num[element]);
+// }
+
+const value = "Hi, This is Nayan Patidar";
+const words = value.split(" ");
+// console.log(words);
+
+const stringUsers = `{"name": "Nayan","age":18,"gender":"male"}`
+const user = JSON.parse(stringUsers)
+console.log(user);
+
+const objUsers = {
+    "name"   : "Harkirat",
+    "age"    : 24,
+    "gender" : "male",
 }
-
-let num = [1,4,0,2,4,9,2,2]
-for (const element in num) {
-    console.log(num[element]);
-}
-
+console.log(objUsers);
