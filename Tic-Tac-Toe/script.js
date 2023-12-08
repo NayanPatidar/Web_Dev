cea3bc17877393b97975264fb5dbee72cf0aef19
Nayan Patidar <nayanpatidar28@gmail.com>
2023-12-08 23:11:33 +0530

    Animation for each cell

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -1,8 +1,17 @@
-function hoverFunction(cellNum) {
+function onHoverFunction(cellNum) {
   console.log(`The cell hovered is ${cellNum}`);
   let cell = document.getElementById(`cell${cellNum}`);
-  cell.style.backgroundColor = 'red';
+  cell.style.opacity = 1;
+  cell.style.backgroundImage = `url('resources/Hover/O_Hover.png')`;;
 
+}
+
+function exitHoverFunction(cellNum) {
+  console.log(`The cell hovered is ${cellNum}`);
+  let cell = document.getElementById(`cell${cellNum}`);
+  setTimeout(() => {
+    cell.style.opacity = 0;
+  }, 1);
 }
 
 function imageSelector() {
