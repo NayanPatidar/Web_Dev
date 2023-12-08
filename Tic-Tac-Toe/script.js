a862e5c9104758d48edf424c69d896c618b1c9c2
Nayan Patidar <nayanpatidar28@gmail.com>
2023-12-08 12:43:20 +0530

    Data for Every Box Added

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -1,7 +1,23 @@
-let valuesArray = [0, 0];
 
-    // Function to update the array value and the box text
-    function updateValue(index) {
-      valuesArray[index]++;
-      document.getElementById(`box${index + 1}`).innerText = `${valuesArray[index]}`;
+let boxData = new Map();
+function updateValue(boxNum){
+  
+  let presence = (checkKeyPresence(boxNum));
+  if (!presence){
+    boxData.set(`${boxNum}`, 1);
+  } else {
+    let va = boxData.get(`${boxNum}`);
+    boxData.set(`${boxNum}`, va+1);
+  }
+
+  document.querySelector(`.box${boxNum + 1}`).innerText = boxData.get(`${boxNum}`);
+}
+
+function checkKeyPresence(boxNum){
+  for (let [key, value] of boxData){
+    if (`${key}` == boxNum){
+      return true;
     }
+  }
+  return false;
+}
