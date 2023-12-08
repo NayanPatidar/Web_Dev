640fbe271505a39e75ec0a26eea65dfe848ee77e
Nayan Patidar <nayanpatidar28@gmail.com>
2023-12-08 22:21:33 +0530

    Hover Function Testing

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -1,23 +1,10 @@
+function hoverFunction(cellNum) {
+  console.log(`The cell hovered is ${cellNum}`);
+  let cell = document.getElementById(`cell${cellNum}`);
+  cell.style.backgroundColor = 'red';
 
-let boxData = new Map();
-function updateValue(boxNum){
-  
-  let presence = (checkKeyPresence(boxNum));
-  if (!presence){
-    boxData.set(`${boxNum}`, 1);
-  } else {
-    let va = boxData.get(`${boxNum}`);
-    boxData.set(`${boxNum}`, va+1);
-  }
-
-  document.querySelector(`.box${boxNum + 1}`).innerText = boxData.get(`${boxNum}`);
 }
 
-function checkKeyPresence(boxNum){
-  for (let [key, value] of boxData){
-    if (`${key}` == boxNum){
-      return true;
-    }
-  }
-  return false;
+function imageSelector() {
+
 }
