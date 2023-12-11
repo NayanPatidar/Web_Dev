cd11d28909f840f86bea211e58c9ce4f63544c45
NayanPatidar <nayanpatidar28@gmail.com>
2023-12-11 16:29:29 +0530

    Timer

--- COHORT/class-2.1/main.js
+++ COHORT/class-2.1/main.js
@@ -26,11 +26,13 @@ function couterTwo(current,time){
         }, 1000);
 }
 
+const { log } = require('console');
 // couterTwo(1,5)
 
 // Reading a file operation
 
 const fs = require('fs');
+const { CLIENT_RENEG_WINDOW } = require('tls');
 
 // fs.readFile('/media/nayan/Core 1/Javascript/Web_Dev/COHORT/class-2.1/Texting.txt', 'utf8', (err, data) => {
 //     if (err) {
@@ -59,26 +61,61 @@ let dataToAdd = "\nYoo keep going !!";
 //     console.log(`File Content: ${data}`);
 // })
 
+
+//Medium
+
 // Remove spaces from file 
 
-let dataFromFiles = "";
-let newData = "";
+// let dataFromFiles = "";
+// let newData = "";
 
-function removeSpaces(inputString) {
-    return inputString.replace(/ +/g, ' ');
+// function removeSpaces(inputString) {
+//     return inputString.replace(/ +/g, ' ');
+// }
+
+// fs.readFile('/media/nayan/Core 1/Javascript/Web_Dev/COHORT/class-2.1/Texting.txt', 'utf8', (error, data) => {
+//     if (error != null){
+//         log.error("We got error");
+//     } else {
+//     dataFromFiles = data;
+//     newData = removeSpaces(dataFromFiles);
+//     console.log(`Data to Add : ${newData}`);
+//     fs.appendFile('/media/nayan/Core 1/Javascript/Web_Dev/COHORT/class-2.1/Texting.txt', newData, (err, data) => { 
+//      });
+//     }
+// })
+
+
+// Using 1-counter.md or 2-counter.md from the easy section, can you create a clock that shows you the current machine time?
+
+// Can you make it so that it updates every second, and shows time in the following formats -
+
+// 1] HH:MM::SS (Eg. 13:45:23)
+
+// 2] HH:MM::SS AM/PM (Eg 01:45:23 PM)
+  
+// 1]
+
+let timeData = [10,59,0];
+console.clear();
+function counter(){
+    let MaxSec = 60;
+    let MaxMin = 60;
+    let Day = 12;
+    let i  = 55;
+        let interval = setInterval(() => {
+            console.clear();
+            timeData[2] = i;
+            console.log(`${timeData[0]}:${timeData[1]}:${timeData[2]}`);
+            i++;
+            if (MaxSec < i){
+                timeData[1] += 1;
+                i = 1; 
+                if (timeData[1] >= 59){
+                    timeData[0] += 1;
+                    timeData[1] = 0;
+                }
+            } 
+        }, 1000);
 }
-
-fs.readFile('/media/nayan/Core 1/Javascript/Web_Dev/COHORT/class-2.1/Texting.txt', 'utf8', (error, data) => {
-    if (error != null){
-        log.error("We got error");
-    } else {
-    dataFromFiles = data;
-    newData = removeSpaces(dataFromFiles);
-    console.log(`Data to Add : ${newData}`);
-    fs.appendFile('/media/nayan/Core 1/Javascript/Web_Dev/COHORT/class-2.1/Texting.txt', newData, (err, data) => {    });
-    }
-})
-
-
-  
-  
+counter();
