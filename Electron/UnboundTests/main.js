5af02a6ee43a9cadddbb1561afd8ab52fd668f16
NayanPatidar <nayanpatidar28@gmail.com>
2023-12-12 22:31:29 +0530

    Created the Window

--- Electron/UnboundTests/main.js
+++ Electron/UnboundTests/main.js
@@ -1,7 +1,22 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
 
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
-        
+        width: 800,
+        height: 400,
+        resizable: false
+    });
+    Menu.setApplicationMenu(null);
+    mainWindow.loadFile(path.join(__dirname, "./src/GUI/index.html"));
+
+    mainWindow.on('close', function(event) {
+        event.preventDefault();
+        mainWindow.hide();
     });
 }
+
+app.whenReady().then(() => {
+    createMainWindow();
+})
+
