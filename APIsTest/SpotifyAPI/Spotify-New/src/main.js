d721eb981bd676c276a9361dd62b0da408fd7496
Nayan Patidar <nayanpatidar28@gmail.com>
2023-12-15 20:24:21 +0530

    Added  the access the tokens

--- APIsTest/SpotifyAPI/Spotify-New/src/main.js
+++ APIsTest/SpotifyAPI/Spotify-New/src/main.js
@@ -17,6 +17,20 @@ app.get('/login', (req, res) => {
     console.log(clientId);
 }); 
 
+app.get('/callback', async (req, res) => {
+    const code = req.query.code || null;
+    const state = req.query.state || null;
+
+    try {
+        const tokenResponse = await exchangeCodeForToken(code);
+        console.log(tokenResponse);
+        res.send('Authentication successful!');
+    } catch (error) {
+        console.error('Token exchange error:', error.message);
+        res.status(500).send('Internal Server Error');
+    }
+})
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
@@ -29,12 +43,30 @@ app.on('error', (err) => {
 function redirectToAuthCodeFlow(res) {  
     res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
-
       response_type: 'code',
       client_id: clientId,
       scope: scopes,
       redirect_uri: redirectUri
-    
     }));
   }
   
+  async function exchangeCodeForToken(code) {
+    const tokenUrl = 'https://accounts.spotify.com/api/token';
+    const authHeader = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
+
+    const data = {
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: redirectUri,
+    };
+
+    const config = {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': `Basic ${authHeader}`,
+        },
+    };
+
+    const response = await axios.post(tokenUrl, querystring.stringify(data), config);
+    return response.data;
+}
