7bf86f15cf6da9e2173b2f654384821a75d465cc
Nayan Patidar <nayanpatidar28@gmail.com>
2024-01-08 18:03:15 +0530

    New Route

--- COHORT/class-2.2/FILESERVER/fileServer.js
+++ COHORT/class-2.2/FILESERVER/fileServer.js
@@ -2,14 +2,56 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const port = 8080;
 
 let files = [];
 
-fs.readdir('FILES', (err, data) => {
-    if(err){
-        console.error(err);
-        return;
-    }
-    files.push(data);
-    console.log(files);
+app.get('/files', (req, res) => {
+    getFilesName()
+        .then(data => {
+            res.status(200).send(data)
+        })
 })
+
+app.get('/file/:filename', (req, res) => {
+    const fileName = req.params.filename;
+    getFileData(fileName)
+        .then(data => {
+            res.status(200).send(data);
+        })
+        .catch(err => {
+            res.status(400).send("File not found");
+        });
+})
+
+app.listen(port, ()=> {
+    console.log(`Listening on the port ${port}`);
+})
+
+
+
+// FS Functions
+
+ function getFilesName(){
+    return new Promise (function(resolve, reject) {
+        fs.readdir('FILES', (err,data) => {
+            if(err){
+                reject(err)
+                return;
+            }
+            resolve(data);
+        })
+    })
+}
+
+function getFileData(fileName){
+    return new Promise(function(resolve, reject) {
+        fs.readFile(`FILES/${fileName}`, 'utf-8', (err, data) => {
+            if(err){
+                reject(err);
+                return;
+            }
+            resolve(data);
+        })
+    })
+}
