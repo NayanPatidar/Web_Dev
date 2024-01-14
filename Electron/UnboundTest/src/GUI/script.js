9e10672516decf95028e7108f962c8b51d82d1f0
Nayan Patidar <nayanpatidar28@gmail.com>
2024-01-14 13:13:46 +0530

    Added Colour Change Property

--- Electron/UnboundTest/src/GUI/script.js
+++ Electron/UnboundTest/src/GUI/script.js
@@ -1,18 +1,27 @@
 console.log("Yooo");
 
-document.addEventListener('DOMContentLoaded', () => {
-    // console.log("In the fullt loaded DOM");
     let buttons = document.querySelectorAll('.features ul li input[type="radio"]');
     // console.log(buttons);
 
     buttons.forEach((button)=> {
-        console.log(button);
-        button.addEventListener('checks', ()=> {
-            if (button.checked){
-                console.log(`${button} is checked`);
-            }else {
-                console.log(`${button} is not checked`);
-            }
-        })
+        button.addEventListener('change', function(){
+
+            let AlllistItems = document.querySelectorAll('.features ul li');
+            AlllistItems.forEach((listItem) => {
+                listItem.style.backgroundColor = '#fff'; 
+                listItem.style.color = '#000'
+            });
+            let listItem = button.closest('li');
+
+                if (button.checked) {
+                    listItem.style.backgroundColor  = '#141321';
+                    listItem.style.color = '#fff'
+                } else {
+                    console.log("Here in else");
+                    listItem.style.backgroundColor  = '#fff';
+                }
+            })  
+
     })
-})
+
+    
