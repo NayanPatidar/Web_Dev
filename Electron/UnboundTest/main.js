331dfb822e29f9f0d143bf2abdbc6792f43b5914
Nayan Patidar <nayanpatidar28@gmail.com>
2024-01-14 13:13:07 +0530

    Added Swtich

--- Electron/UnboundTest/main.js
+++ Electron/UnboundTest/main.js
@@ -73,3 +73,4 @@ app.on('activate', function () {
 		mainWindow.show();
 	}
 })
+
--- Electron/UnboundTest/src/GUI/script.js
+++ Electron/UnboundTest/src/GUI/script.js
@@ -1,1 +1,18 @@
 console.log("Yooo");
+
+document.addEventListener('DOMContentLoaded', () => {
+    // console.log("In the fullt loaded DOM");
+    let buttons = document.querySelectorAll('.features ul li input[type="radio"]');
+    // console.log(buttons);
+
+    buttons.forEach((button)=> {
+        console.log(button);
+        button.addEventListener('checks', ()=> {
+            if (button.checked){
+                console.log(`${button} is checked`);
+            }else {
+                console.log(`${button} is not checked`);
+            }
+        })
+    })
+})
