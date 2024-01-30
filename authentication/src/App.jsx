30dc25ea29027faec1c8bb468205fcb4de0ea994
Nayan Patidar <nayanpatidar28@gmail.com>
2024-01-30 23:13:51 +0530

    Background and ShadUI

--- authentication/src/App.jsx
+++ authentication/src/App.jsx
@@ -2,7 +2,17 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <img
+        src="/assets/images/background.jpg"
+        className="w-full h-screen object-cover brightness-75"
+      />
+      <div className="absolute inset-0 flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-white">Your Text Here</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
