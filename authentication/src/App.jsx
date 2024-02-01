5b28e82dd2834621a2abb3421abcdde798eaa8c6
Nayan Patidar <nayanpatidar28@gmail.com>
2024-02-01 18:32:52 +0530

    inputChange Func and useState

--- authentication/src/App.jsx
+++ authentication/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -6,13 +6,20 @@ import { z } from "zod";
 import "./App.css";
 
 function App() {
-  function onSubmit(values) {
-    console.log(values);
-  }
-
-  const formSchema = z.object({
-    username: z.string().min(2).max(50),
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
   });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
   return (
     <>
@@ -27,29 +34,52 @@ function App() {
           <div className=" w-11/12 h-max m-3 text-black text-sm p-2">
             <div className="flex flex-col pb-2">
               <label className=" text-white pb-1 ">Name</label>
-              <input type="text" className="rounded h-8 pl-1"></input>
+              <input
+                type="text"
+                name="username"
+                className="rounded h-8 pl-1"
+                onChange={handleInputChange}
+                value={formData.username}
+              />
             </div>
 
             <div className="flex flex-col pb-2">
               <label className=" text-white pb-1">Email</label>
-              <input type="email" className="rounded h-8 pl-1"></input>
+              <input
+                type="email"
+                name="email"
+                className="rounded h-8 pl-1"
+                onChange={handleInputChange}
+                value={formData.email}
+              />
             </div>
 
             <div className="flex flex-col pb-2">
               <label className=" text-white pb-1">Password</label>
-              <input type="password" className="rounded h-8 pl-1"></input>
+              <input
+                type="password"
+                name="password"
+                className="rounded h-8 pl-1"
+                onChange={handleInputChange}
+                value={formData.password}
+              />
             </div>
 
             <div className="flex flex-col pb-2">
               <label className="text-white pb-1">Confirm Password</label>
-              <input type="password" className="rounded h-8 pl-1"></input>
+              <input
+                type="password"
+                name="confirmPassword"
+                className="rounded h-8 pl-1"
+                onChange={handleInputChange}
+                value={formData.confirmPassword}
+              />
             </div>
 
             <button
               type="submit"
               className=" w-full bg-black text-white px-4 py-2 rounded-md mt-8 hover:underline "
             >
-              {" "}
               Sign Up
             </button>
           </div>
