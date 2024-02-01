e299f85f78078fce58d0976e86d65df7e1f712c0
Nayan Patidar <nayanpatidar28@gmail.com>
2024-02-01 16:54:59 +0530

    Adding the forms

--- authentication/src/App.jsx
+++ authentication/src/App.jsx
@@ -5,27 +5,7 @@ import { z } from "zod";
 
 import "./App.css";
 
-import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-
 function App() {
-  
-  const form = useForm({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-    },
-  });
-
   function onSubmit(values) {
     console.log(values);
   }
@@ -37,29 +17,32 @@ function App() {
   return (
     <>
       <div className="absolute z-10 flex items-center justify-center align-middle w-screen h-screen">
-        <h1 className="text-4xl font-bold text-white">
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Username</FormLabel>
-                    <FormControl>
-                      <Input placeholder="shadcn" {...field} />
-                    </FormControl>
-                    <FormDescription>
-                      This is your public display name.
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button type="submit">Submit</Button>
-            </form>
-          </Form>
-        </h1>
+        <div className=" bg-gray-400 bg-opacity-50 w-80 h-96 min-w-64  border-2  border-solid border-gray-200 rounded-lg flex flex-col justify-start items-center p-3">
+          <h1 className=" text-2xl font-bold text-white flex justify-center">
+            Register
+          </h1>
+          <div className=" w-11/12 h-3/5 m-3 text-black text-sm p-2">
+            <div className="flex flex-col">
+              <label className=" text-white">Name</label>
+              <input type="text" className="rounded"></input>
+            </div>
+
+            <div className="flex flex-col">
+              <label className=" text-white">Email</label>
+              <input type="email" className="rounded"></input>
+            </div>
+
+            <div className="flex flex-col">
+              <label className=" text-white">Password</label>
+              <input type="password" className="rounded"></input>
+            </div>
+
+            <div className="flex flex-col">
+              <label className="text-white">Confirm Password</label>
+              <input type="password" className="rounded"></input>
+            </div>
+          </div>
+        </div>
       </div>
       <img
         src="/assets/images/background.jpg"
