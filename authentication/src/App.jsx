17b7d899e1ef5632e30c214ea232188f95840f7b
Nayan Patidar <nayanpatidar28@gmail.com>
2024-02-01 22:38:01 +0530

    Added the validation logic

--- authentication/src/App.jsx
+++ authentication/src/App.jsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
 
 import "./App.css";
 
 function App() {
   const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [formError, setFormError] = useState({
     username: "",
     email: "",
     password: "",
@@ -19,69 +23,105 @@ function App() {
       ...formData,
       [name]: value,
     });
+    console.log(formError[name]);
+    formError[name] = "";
+  };
+
+  // const checkConfirmPassword = () => {};
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormError(validateInput(formData));  
+
+  };
+
+  const validateInput = (values) => {
+    const errors = {}
+
+    if (!values.username) {
+      errors.username = "Username is required !"
+    }
+
+    if (!values.email) {
+      errors.email = "Email is required !"
+    }
+
+    if (!values.password) {
+      errors.password = "Password is required !"
+    }
+
+    if (!values.confirmPassword) {
+      errors.confirmPassword = "Confirm the password !"
+    }
+
+    return errors;
   };
 
   return (
     <>
       <div className="absolute z-10 flex items-center justify-center align-middle w-screen h-screen">
-        <div
-          style={{ height: "425px" }}
-          className=" flex flex-col justify-start items-center bg-gray-400 bg-opacity-50 w-80 min-w-64  border-2  border-solid border-gray-200 rounded-lg p-3"
-        >
+        <div className=" flex flex-col justify-start items-center bg-gray-400 bg-opacity-50 w-80 h-auto min-w-64  border-2  border-solid border-gray-200 rounded-lg p-3">
           <h1 className=" text-2xl font-bold text-white flex justify-center">
             Register
           </h1>
+
           <div className=" w-11/12 h-max m-3 text-black text-sm p-2">
-            <div className="flex flex-col pb-2">
-              <label className=" text-white pb-1 ">Name</label>
-              <input
-                type="text"
-                name="username"
-                className="rounded h-8 pl-1"
-                onChange={handleInputChange}
-                value={formData.username}
-              />
-            </div>
-
-            <div className="flex flex-col pb-2">
-              <label className=" text-white pb-1">Email</label>
-              <input
-                type="email"
-                name="email"
-                className="rounded h-8 pl-1"
-                onChange={handleInputChange}
-                value={formData.email}
-              />
-            </div>
-
-            <div className="flex flex-col pb-2">
-              <label className=" text-white pb-1">Password</label>
-              <input
-                type="password"
-                name="password"
-                className="rounded h-8 pl-1"
-                onChange={handleInputChange}
-                value={formData.password}
-              />
-            </div>
-
-            <div className="flex flex-col pb-2">
-              <label className="text-white pb-1">Confirm Password</label>
-              <input
-                type="password"
-                name="confirmPassword"
-                className="rounded h-8 pl-1"
-                onChange={handleInputChange}
-                value={formData.confirmPassword}
-              />
-            </div>
-
-            <button
-              type="submit"
-              className=" w-full bg-black text-white px-4 py-2 rounded-md mt-8 hover:underline "
-            >
-              Sign Up
-            </button>
+            <form onSubmit={handleSubmit} noValidate>
+              <div className="flex flex-col pb-2">
+                <label className=" text-white pb-1 ">Name</label>
+                <input
+                  type="text"
+                  name="username"
+                  className="rounded h-8 pl-1"
+                  onChange={handleInputChange}
+                  value={formData.username}
+                  required
+                />
+                <p className="text-red-600">{formError.username}</p>
+              </div>
+              <div className="flex flex-col pb-2">
+                <label className=" text-white pb-1">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  className="rounded h-8 pl-1"
+                  onChange={handleInputChange}
+                  value={formData.email}
+                  required
+                />
+                <p className="text-red-600">{formError.email}</p>
+              </div>
+              <div className="flex flex-col pb-2">
+                <label className=" text-white pb-1">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  className="rounded h-8 pl-1"
+                  onChange={handleInputChange}
+                  value={formData.password}
+                  required
+                />
+                <p className="text-red-600">{formError.password}</p>
+              </div>
+              <div className="flex flex-col pb-2">
+                <label className="text-white pb-1">Confirm Password</label>
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  className="rounded h-8 pl-1"
+                  onChange={handleInputChange}
+                  value={formData.confirmPassword}
+                  required
+                />
+                <p className="text-red-900 ">{formError.confirmPassword}</p>
+              </div>
+              <button
+                type="submit"
+                className=" w-full bg-black text-white px-4 py-2 rounded-md mt-8 hover:underline "
+              >
+                Sign Up
+              </button>
+            </form>
           </div>
         </div>
       </div>
