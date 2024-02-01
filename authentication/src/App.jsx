652c610901f69c6bb1ce71335e67d097d312185d
Nayan Patidar <nayanpatidar28@gmail.com>
2024-02-01 17:22:52 +0530

    Added Button

--- authentication/src/App.jsx
+++ authentication/src/App.jsx
@@ -17,30 +17,41 @@ function App() {
   return (
     <>
       <div className="absolute z-10 flex items-center justify-center align-middle w-screen h-screen">
-        <div className=" bg-gray-400 bg-opacity-50 w-80 h-96 min-w-64  border-2  border-solid border-gray-200 rounded-lg flex flex-col justify-start items-center p-3">
+        <div
+          style={{ height: "425px" }}
+          className=" flex flex-col justify-start items-center bg-gray-400 bg-opacity-50 w-80 min-w-64  border-2  border-solid border-gray-200 rounded-lg p-3"
+        >
           <h1 className=" text-2xl font-bold text-white flex justify-center">
             Register
           </h1>
-          <div className=" w-11/12 h-3/5 m-3 text-black text-sm p-2">
-            <div className="flex flex-col">
-              <label className=" text-white">Name</label>
-              <input type="text" className="rounded"></input>
+          <div className=" w-11/12 h-max m-3 text-black text-sm p-2">
+            <div className="flex flex-col pb-2">
+              <label className=" text-white pb-1 ">Name</label>
+              <input type="text" className="rounded h-8 pl-1"></input>
             </div>
 
-            <div className="flex flex-col">
-              <label className=" text-white">Email</label>
-              <input type="email" className="rounded"></input>
+            <div className="flex flex-col pb-2">
+              <label className=" text-white pb-1">Email</label>
+              <input type="email" className="rounded h-8 pl-1"></input>
             </div>
 
-            <div className="flex flex-col">
-              <label className=" text-white">Password</label>
-              <input type="password" className="rounded"></input>
+            <div className="flex flex-col pb-2">
+              <label className=" text-white pb-1">Password</label>
+              <input type="password" className="rounded h-8 pl-1"></input>
             </div>
 
-            <div className="flex flex-col">
-              <label className="text-white">Confirm Password</label>
-              <input type="password" className="rounded"></input>
+            <div className="flex flex-col pb-2">
+              <label className="text-white pb-1">Confirm Password</label>
+              <input type="password" className="rounded h-8 pl-1"></input>
             </div>
+
+            <button
+              type="submit"
+              className=" w-full bg-black text-white px-4 py-2 rounded-md mt-8 hover:underline "
+            >
+              {" "}
+              Sign Up
+            </button>
           </div>
         </div>
       </div>
