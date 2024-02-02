adad0fa3889e76c3340edc7acb022d0979554692
Nayan Patidar <nayanpatidar28@gmail.com>
2024-02-03 01:04:50 +0530

    Firebase Authentication

--- firebase/src/components/Signup.jsx
+++ firebase/src/components/Signup.jsx
@@ -1,10 +1,18 @@
 import React, { useRef } from "react";
 import { Card, Form, Button } from "react-bootstrap";
+import { useAuth } from "../context/AuthContext";
 
 export default function Signup() {
   const emailRef = useRef();
   const passowrdRef = useRef();
   const passowrdConfirmationRef = useRef();
+  const { signup } = useAuth();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    signup(emailRef.current.value, passowrdRef.current.value);
+  }
+
   return (
     <>
       <Card>
@@ -16,7 +24,7 @@ export default function Signup() {
               <Form.Control type="email" ref={emailRef} required />
             </Form.Group>
             <Form.Group id="password">
-              <Form.Label>Email</Form.Label>
+              <Form.Label>Password</Form.Label>
               <Form.Control type="password" ref={passowrdRef} required />
             </Form.Group>
             <Form.Group id="password-confirm">
@@ -27,7 +35,7 @@ export default function Signup() {
                 required
               />
             </Form.Group>
-            <Button type="submit" className="w-100 h-100">
+            <Button type="submit" className="w-100 h-100 mt-4">
               Sign Up
             </Button>
           </Form>
