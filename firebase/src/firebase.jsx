9acdf3ef79f7e0686bc5741dc6db6abc5ef4b3ed
Nayan Patidar <nayanpatidar28@gmail.com>
2024-02-04 14:04:50 +0530

    Added the firestore to vite

--- firebase/src/firebase.jsx
+++ firebase/src/firebase.jsx
@@ -1,15 +1,15 @@
-import firebase from "firebase/app"
-import "firebase/auth"
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
-})
+const firebaseConfig = {
+  apiKey: "AIzaSyAjeJogf1ZM3VwvLOj2eOGp9ccgnPqHcmg",
+  authDomain: "test-c9413.firebaseapp.com",
+  projectId: "test-c9413",
+  storageBucket: "test-c9413.appspot.com",
+  messagingSenderId: "1015312253904",
+  appId: "1:1015312253904:web:bc9acf93b121846d2eefd5",
+  measurementId: "G-4G8TCBD9R5"
+};
 
-export const auth = app.auth()
-export default app
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
