c47e7e04a6da28cd6d3f1015ba1e62ea799e7afd
Nayan Patidar <nayanpatidar28@gmail.com>
2024-02-04 23:18:24 +0530

    Firbase Fixes

--- firebase/Firebase_React_Auth/src/components/App.jsx
+++ firebase/Firebase_React_Auth/src/components/App.jsx
@@ -2,19 +2,20 @@ import { useState } from "react";
 import "./App.css";
 import Signup from "./Signup";
 import { Container } from "react-bootstrap";
+import { AuthProvider } from "../context/AuthContext";
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <Container
         className="flex align-items-center justify-center"
         style={{ minHeight: "100vh" }}
       >
-        <div className="w-100" style={{ maxWidth: "400px"}}>
+        <div className="w-100" style={{ maxWidth: "400px" }}>
           <Signup />
         </div>
       </Container>
-    </>
+    </AuthProvider>
   );
 }
 
--- firebase/Firebase_React_Auth/src/components/Signup.jsx
+++ firebase/Firebase_React_Auth/src/components/Signup.jsx
@@ -1,33 +1,55 @@
-import React, { useRef } from "react";
-import { Card, Form, Button } from "react-bootstrap";
+import React, { useRef, useState } from "react";
+import { Card, Form, Button, Alert } from "react-bootstrap";
+import { useAuth } from "../context/AuthContext";
 
 export default function Signup() {
-  const emailRef = useRef();
-  const passowrdRef = useRef();
-  const passowrdConfirmationRef = useRef();
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const passwordConfirmRef = useRef()
+  const { signup } = useAuth()
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (
+      passwordRef.current.value !== passwordConfirmRef.current.value
+    ) {
+      return setError("Passwords Do not match");
+    }
+
+    try {
+      setError("");
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+    } catch (error) {
+      setError(`Failed to create an account:, ${error.message} `);
+      console.error(` ${error.message}`);
+    }
+
+    setLoading(false);
+  }
   return (
     <>
       <Card>
         <Card.Body>
-          <h2 className=" text-center mb-4">Sign Up</h2>
-          <Form>
+          <h2 className="text-center mb-4">Sign Up</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" ref={emailRef} required />
             </Form.Group>
             <Form.Group id="password">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="password" ref={passowrdRef} required />
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" ref={passwordRef} required />
             </Form.Group>
             <Form.Group id="password-confirm">
               <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control
-                type="password"
-                ref={passowrdConfirmationRef}
-                required
-              />
+              <Form.Control type="password" ref={passwordConfirmRef} required />
             </Form.Group>
-            <Button type="submit" className="mt-3 w-100 h-100">
+            <Button disabled={loading} className="w-100" type="submit">
               Sign Up
             </Button>
           </Form>
