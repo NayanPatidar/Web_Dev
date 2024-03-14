958385be7e7ebd7c93f6578c6df02b0db73f06c1
Nayan Patidar <nayanpatidar28@gmail.com>
2024-03-14 23:43:51 +0530

    Add CheckUser Function

--- Backend/Postgres/route.js
+++ Backend/Postgres/route.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const { AddUser } = require("./services");
+const { AddUser, CheckUser } = require("./services");
 
 const PORT = 8080;
 app.use(bodyParser.json());
 
-app.get("/user/signin", (req, res) => {
+app.get("/user/signin", async (req, res) => {
   const { name, password } = req.body;
   console.log(`${name} tried to sign in with the password ${password}`);
-  AddUser(name, password);
+
+  const userId = await CheckUser(name, password);
+  // if (userId )
+  console.log("User ID:", userId);
+
   res.send("Sign In Request has been made !!");
 });
 
--- Backend/Postgres/services.js
+++ Backend/Postgres/services.js
@@ -1,9 +1,16 @@
 const { client } = require("./pg_client");
 
-async function AddUser(username, password) {
-  const pg_query = ` INSERT INTO users (name , password) VALUES ($1 , $2)`;
-  const result = await client.query(pg_query, [username, password]);
+async function AddUser(username, password, email) {
+  const pg_query = ` INSERT INTO users (name , password, email) VALUES ($1 , $2, $3)`;
+  const result = await client.query(pg_query, [username, password, email]);
   return result;
 }
 
-module.exports = { AddUser };
+async function CheckUser(username, email){
+  const pg_query = ` SELECT user_id FROM users WHERE name = ($1) OR email = ($2) `
+  const result = await client.query(pg_query, [username, email]);
+  const userId = result.rows[0]?.user_id;
+  return userId;
+}
+
+module.exports = { AddUser, CheckUser };
