85c538aa170cbb1ccb53b3bc9f19a288e35ecd50
Nayan Patidar <nayanpatidar28@gmail.com>
2024-03-14 23:59:31 +0530

    Minor Updates

--- Backend/Postgres/route.js
+++ Backend/Postgres/route.js
@@ -7,14 +7,19 @@ const PORT = 8080;
 app.use(bodyParser.json());
 
 app.get("/user/signin", async (req, res) => {
-  const { name, password } = req.body;
+  const { name, password, email } = req.body;
   console.log(`${name} tried to sign in with the password ${password}`);
 
-  const userId = await CheckUser(name, password);
-  // if (userId )
+  const userId = await CheckUser(name, email);
   console.log("User ID:", userId);
-
-  res.send("Sign In Request has been made !!");
+  if (userId == undefined || userId == 0) {
+    AddUser(name, password, email);
+    res.send("No Accounts Exists! So User Has Been Added !!!");
+    return;
+  } else {
+    res.send("User has been Signed In !!!");
+    return;
+  }
 });
 
 app.listen(PORT, () => console.log(`Server is running on the port ${PORT}`));
