c62819521ade0e2b3f9d78f973031e83947eaf7e
Nayan Patidar <nayanpatidar28@gmail.com>
2024-04-27 22:18:00 +0530

    Enum

--- typescript/a.js
+++ typescript/a.js
@@ -1,7 +1,24 @@
 "use strict";
 let a = 3;
-console.log(a);
+// console.log(a);
 function greet(firstName) {
     console.log(`Hello ${firstName}`);
 }
-greet("Nayan");
+// greet("Nayan");
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 0] = "Up";
+    Direction[Direction["Down"] = 1] = "Down";
+    Direction[Direction["Left"] = 2] = "Left";
+    Direction[Direction["Right"] = 3] = "Right";
+})(Direction || (Direction = {}));
+function doSomething(keyPressed) {
+    if (keyPressed == Direction.Right) {
+        console.log(keyPressed);
+    }
+}
+function firstElement(arr) {
+    return arr[0];
+}
+const value = firstElement(["Nayan", "Patidar"]);
+console.log(value);
--- typescript/a.ts
+++ typescript/a.ts
@@ -1,8 +1,32 @@
 let a: number = 3;
-console.log(a);
+// console.log(a);
 
 function greet(firstName: string) {
   console.log(`Hello ${firstName}`);
 }
 
-greet("Nayan");
+// greet("Nayan");
+
+enum Direction {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+
+function doSomething(keyPressed: Direction) {
+  if (keyPressed == Direction.Right) {
+    console.log(keyPressed);
+  }
+}
+
+// doSomething(Direction.Right);
+
+type Input = string | number;
+
+function firstElement(arr: Input[]): Input {
+  return arr[0];
+}
+
+const value = firstElement(["Nayan", "Patidar"]);
+// console.log();
