f68044ba41652bf5e87f45dbc2b9a1225c1b5b82
Nayan Patidar <nayanpatidar28@gmail.com>
2024-04-27 21:54:21 +0530

    Type Script Basics

---
+++ typescript/a.js
@@ -0,0 +1,7 @@
+"use strict";
+let a = 3;
+console.log(a);
+function greet(firstName) {
+    console.log(`Hello ${firstName}`);
+}
+greet("Nayan");
---
+++ typescript/a.ts
@@ -0,0 +1,8 @@
+let a: number = 3;
+console.log(a);
+
+function greet(firstName: string) {
+  console.log(`Hello ${firstName}`);
+}
+
+greet("Nayan");
