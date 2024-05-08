c305f69330b34875c8597a52235f3199d485215c
Nayan Patidar <nayanpatidar28@gmail.com>
2024-05-08 23:49:35 +0530

    Request Method Check

--- CloudFlareWorker/my-app/src/index.ts
+++ CloudFlareWorker/my-app/src/index.ts
@@ -1,18 +1,21 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Response } from '@cloudflare/workers-types/experimental';
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		console.log(request.body);
+		console.log(request.headers);
+		console.log(request.method);
+
+		if (request.method === 'GET') {
+			return Response.json({
+				Message: 'Get Request Send',
+			});
+		} else {
+			return Response.json({
+				Message: 'Not a Get Request Send ',
+			});
+		}
+
 		return new Response('Hello World!');
 	},
 };
