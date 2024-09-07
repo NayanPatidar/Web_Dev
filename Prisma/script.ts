a7f1deb4c7bad072a537c83eb5930cc4f1e4c0e8
Nayan Patidar <nayanpatidar28@gmail.com>
2024-09-07 17:05:40 +0530

    Chech User Presence

--- Prisma/script.ts
+++ Prisma/script.ts
@@ -2,12 +2,33 @@ import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv";
 
 dotenv.config();
-
 const prisma = new PrismaClient();
 
 async function main() {
-  const user = await prisma.user.create({ data: { name: "Nayan" } });
-  console.log(user);
+  const email = "nayanpatidar28@gmail.com";
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email: email, // Use the email to find the unique user
+    },
+  });
+
+  if (existingUser) {
+    console.log("User with this email already exists : ", existingUser);
+    return;
+  }
+
+  const newUser = await prisma.user.create({
+    data: {
+      name: "Nayan",
+      email: email,
+    },
+    select: {
+      name: true,
+      email: true,
+    },
+  });
+
+  console.log("Created user:", newUser);
 }
 
 main()
