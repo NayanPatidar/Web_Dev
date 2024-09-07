f53f70de6b1530e8ee0195effef97c135dfaeee4
Nayan Patidar <nayanpatidar28@gmail.com>
2024-09-07 13:20:36 +0530

    Playlist and Liked Songs Schema

---
+++ Prisma/script.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({ data: { name: "Nayan" } });
+  console.log(user);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
